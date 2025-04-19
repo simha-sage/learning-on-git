b26b0026ee0e9570fb42e401c87667c4dbf5f115
lakshmi Narasimha <lakshminarasimha33333@gmail.com>
2025-04-19 11:43:54 +0530

    added_find()

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const User=require("./user.js")
 app.use(express.json())
 
 app.post("/signup",async (req,res)=>{
-    const user= new User({
-        firstName:"munna",
-        lastName:"bhai",
-        email:"munna@gmail.com"
-    })
+    const user= new User(req.body)
     await user.save()
+    
     res.send("added")
+})
+
+app.get("/feed",async(req,res)=>{
+    const emailid=req.body.email
+    const user=await User.find({email:emailid})
+    res.send(user)
 })
 
 db().then(()=>{
